677e4fb47fc2167daa1cffef49b2666620e9e0aa
KimsPractice <38434350+KimsPractice@users.noreply.github.com>
2019-09-17 03:26:57 +0900

    TS Fundamental

--- Fundamental/dist/index.js
+++ Fundamental/dist/index.js
@@ -1,2 +1,23 @@
-console.log("Hello World");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const person = {
+    name: "Kimspractice",
+    age: 27,
+    gender: "male"
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age} you are a ${person.gender}`;
+};
+const sayHi2 = (person) => {
+    return `Hello ${person.name}, you are ${person.age} you are a ${person.gender}`;
+};
+console.log(sayHi(person));
+console.log(sayHi2(person));
 //# sourceMappingURL=index.js.map
--- Fundamental/src/index.ts
+++ Fundamental/src/index.ts
@@ -1,1 +1,34 @@
-console.log("Hello World");
+interface Human1 {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const person = {
+  name: "Kimspractice",
+  age: 27,
+  gender: "male"
+};
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age} you are a ${person.gender}`;
+};
+const sayHi2 = (person: Human1): string => {
+  return `Hello ${person.name}, you are ${person.age} you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
+console.log(sayHi2(person));
+
+export {};
