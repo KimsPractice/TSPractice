1f6e3b38eaa5f4767655d1d93657e05cc9e57c9a
KimsPractice <38434350+KimsPractice@users.noreply.github.com>
2019-09-17 06:47:03 +0900

    First TS mini BlockChain

--- Fundamental/dist/index.js
+++ Fundamental/dist/index.js
@@ -1,23 +1,64 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+const CryptoJS = require("crypto-js");
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const person = {
-    name: "Kimspractice",
-    age: 27,
-    gender: "male"
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.data === "string" &&
+    typeof aBlock.timestamp === "number";
+const genesisBlock = new Block(0, "12313020202", "", "Hello", 123465);
+let blockChain = [genesisBlock];
+const getBlockChain = () => blockChain;
+const getLatestBlock = () => blockChain[blockChain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
 };
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age} you are a ${person.gender}`;
+const getHashforBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+const isNewBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        console.log("validateStructure err");
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        console.log("index err");
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        console.log("hash err");
+        return false;
+    }
+    else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        console.log("equal hash");
+        return false;
+    }
+    else {
+        return true;
+    }
 };
-const sayHi2 = (person) => {
-    return `Hello ${person.name}, you are ${person.age} you are a ${person.gender}`;
+const addBlock = (candidateBlock) => {
+    if (isNewBlockValid(candidateBlock, getLatestBlock())) {
+        blockChain.push(candidateBlock);
+    }
 };
-console.log(sayHi(person));
-console.log(sayHi2(person));
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
+console.log(blockChain);
 //# sourceMappingURL=index.js.map
